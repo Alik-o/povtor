1fb12b02c9fed2f94bbc93affbfbc8cbc51199f3
Олег Навоев <navoev.oleg@gmail.com>
2025-11-27 20:42:16 +0500

    добавил чтение файлов с помощью pandas

--- src/utils.py
+++ src/utils.py
@@ -2,6 +2,8 @@ import json
 import logging
 import os
 
+import pandas as pd
+
 from config import DATA_DIR, LOG_DIR
 
 path_log = os.path.join(LOG_DIR, "utils.log")
@@ -20,10 +22,19 @@ path = os.path.join(DATA_DIR, "operations.json")
 def create_dir(dir_path: str = path):
     """Возвращает список словарей из json файла"""
     try:
-        utils_logger.debug(f"Пытаемся прочитать файл {dir_path}")
-        with open(dir_path, "r", encoding="utf-8") as f:
-            operations = json.load(f)
-        utils_logger.debug(f"Успешно прочитан файл {dir_path}")
+        if dir_path.endswith(".json"):
+            utils_logger.debug(f"Пытаемся прочитать json файл {dir_path}")
+            with open(dir_path, "r", encoding="utf-8") as f:
+                operations = json.load(f)
+            utils_logger.debug(f"Успешно прочитан файл {dir_path}")
+        elif dir_path.endswith(".xlsx"):
+            utils_logger.debug(f"Пытаемся прочитать xlsx файл {dir_path}")
+            operations = pd.read_excel(dir_path)
+            utils_logger.debug(f"Успешно прочитан файл {dir_path}")
+        elif dir_path.endswith(".csv"):
+            utils_logger.debug(f"Пытаемся прочитать csv файл {dir_path}")
+            operations = pd.read_csv(dir_path)
+            utils_logger.debug(f"Успешно прочитан файл {dir_path}")
         return operations
     except Exception as e:
         utils_logger.error(f"Ошибка при чтении файла: {e}")
